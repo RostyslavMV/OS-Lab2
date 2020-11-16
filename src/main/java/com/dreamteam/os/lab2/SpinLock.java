e947e0426c717b399e16e23f37b9335cc530e856
Julia0212 <yulia.samartseva@gmail.com>
2020-11-16 23:07:42 +0200

    implement spin lock

--- src/main/java/com/dreamteam/os/lab2/SpinLock.java
+++ src/main/java/com/dreamteam/os/lab2/SpinLock.java
@@ -1,4 +1,41 @@
 package com.dreamteam.os.lab2;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
+public class SpinLock implements Lock {
+  private AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+  @Override
+  public void lock() {
+    Thread thread = Thread.currentThread();
+    while (!atomicReference.compareAndSet(null, thread)) {}
+  }
+
+  @Override
+  public void lockInterruptibly() throws InterruptedException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean tryLock() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void unlock() {
+    atomicReference.compareAndSet(Thread.currentThread(), null);
+  }
+
+  @Override
+  public Condition newCondition() {
+    throw new UnsupportedOperationException();
+  }
+}
